1f594a63d1ae584a1fde4be5abe6628fe9d4b32d
Kashaf <aimankashaf33@gmail.com>
2021-11-24 20:49:13 +0530

    third commit

--- server/Index.js
+++ server/Index.js
@@ -3,9 +3,12 @@ import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
 import postRoutes from './routes/posts.js';
+import dotenv from 'dotenv';
 
 
 const app=express();
+dotenv.config();
+
 
 
 
@@ -13,12 +16,12 @@ app.use(bodyParser.json({limit: "30mb", extended:true}));
 app.use(bodyParser.urlencoded({limit: "30mb", extended:true}));
 app.use(cors());
 app.use('/posts',postRoutes);
-const CONNECTION_URL='//mongodb+srv://codingsquad:Project123@cluster0.jfujl.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+//const CONNECTION_URL='mongodb+srv://codingsquad:Project123@cluster0.jfujl.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
 //mongodb+srv://AimanKashaf:aimankashaf123@cluster0.uzl8d.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
 
-const PORT=process.env.PORT ||5000;
+const PORT=process.env.PORT || 5000;
 
-mongoose.connect(CONNECTION_URL, {useNewUrlParser:true, useUnifiedTopology:true})
+mongoose.connect(process.env.CONNECTION_URL, {useNewUrlParser:true, useUnifiedTopology:true})
  .then(()=>app.listen(PORT,()=>console.log(`Server running on port: ${PORT}`)))
  .catch((error)=>console.log(error.message));
 
