c43bea48555fc371dd5e0b794e7f3dc8edaa6e8a
Kashaf <aimankashaf33@gmail.com>
2021-11-24 19:46:07 +0530

    Additional Changes

--- client/src/components/Posts/Post/Post.js
+++ client/src/components/Posts/Post/Post.js
@@ -31,13 +31,13 @@ const Post=({post,setCurrentId})=> {
             </div>
             <Typography className={classes.title}  variant="h5" gutterBottom>{post.title}</Typography>
             <CardContent>
-                <Typography  variant="h5" gutterBottom>{post.message}</Typography>
+                <Typography  variant="body2" color="textSeconda" component="p">{post.message}</Typography>
             </CardContent>
 
             <CardActions className={classes.cardActions}>
                 <Button size='small' color='primary' onClick={()=>dispatch(likePost(post._id))}>
                     <ThumbUpAltIcon fontSize="small"/>
-                    Like
+                    &nbsp; Like &nbsp;
                     {post.likeCount}
                 </Button>
                 <Button size='small' color='primary' onClick={()=>dispatch(deletePost(post._id))}>
